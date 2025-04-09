7276fc60b44f1f48b78274f60d2fc30617af0797
Kristine Segovia <kristinesegovia23@gmail.com>
2025-04-09 20:37:50 +0100

    database edit

--- final_project/settings.py
+++ final_project/settings.py
@@ -96,7 +96,6 @@ DATABASES = {
     'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
 
-print("DATABASE_URL:", os.getenv('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- homepage/static/blog/main.js
+++ homepage/static/blog/main.js
@@ -1,4 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // NAVIGATION BAR
+  const toggler = document.querySelector(".navbar-toggler");
+  const collapse = document.querySelector("#navbarToggle");
+  const closeBtn = document.querySelector("#closeSidebar");
+
+
+  toggler.addEventListener("click", function() {
+      collapse.classList.toggle("show");
+  });
+
+  closeBtn.addEventListener("click", function () {
+      collapse.classList.remove("show");
+      collapse.style.width = '0';
+      collapse.style.display = 'none';
+  });
+
+  // Close sidebar when clicking outside of it
+  document.addEventListener("click", function (event) {
+      if (!collapse.contains(event.target) && !toggler.contains(event.target)) {
+          collapse.classList.remove("show");
+      }
+  });
+
+
   // Ensure the "Generate" button fetches a prompt
   let generateBtn = document.getElementById("generate-btn");
   if (generateBtn) {
